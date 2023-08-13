71a025506576ef9ee884dfdf6fa1c9b663474ce5
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-08-13 15:27:16 +0300

    new

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,20 +3,19 @@ import React from "react";
 export default function Posts() {
     return(
         <div>
-           <div className="form"> 
+        <div className="form"> 
                 <input type="text" placeholder="Enter Post Title" />
                 <input type="text" placeholder="Enter Post Desc" />
                 <button>Add Post</button>
-           </div>
-           <div className="posts">
+        </div>
+        <div className="posts">
                 <div className="post">
                     <h2>Post Title</h2>
                     <h2>Post Desc</h2>
                     <button>Edit</button>
                     <button>Delete</button>
                 </div>
-           </div>
+        </div>
         </div>
     )
 }
- 
