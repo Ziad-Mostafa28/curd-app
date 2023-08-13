eb0d6eab838459a4c98c648de68611fbd33f788e
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-08-13 16:13:10 +0300

    new edit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React from "react";
 import './App.css';
-import Posts from './components/Posts';
-import  "./App.css"
+import CourseForm from "./components/CourseForm";
+import CourseList from "./components/CourseList";
 
 function App() {
+
   return (
-    <div className="App">
-      
-      <Posts />
-    </div>
+    <section className="App">
+    <h2>Add Course </h2>
+    <CourseForm/>
+
+    <CourseList/>
+    </section>
   );
 }
 
